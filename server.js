203b72a07ca123aa09f6dc0a52d1b40381be2953
Colby Mclaughlin <colbymclaughlin@MacBook-Pro-132.local>
2026-03-16 07:20:50 -0500

    Fix duplicate PORT declaration

--- server.js
+++ server.js
@@ -75,8 +75,6 @@ app.post("/create-mockup", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
